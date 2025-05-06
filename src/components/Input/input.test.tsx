bb6317d56768c241d196b998a3b6e4332bcadc00
Victor Matheus <victormatheus507@gmail.com>
2025-05-06 16:01:41 -0300

    get and query

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -1,8 +1,10 @@
-import {render} from '@testing-library/react-native';
+import { render, screen } from '@testing-library/react-native';
 import { Input } from '.';
 
 describe('Component: Input', () => {
   it('should be render without activity indicator', () => {
     render(<Input />);
+    const activityIndicator = screen.queryByTestId('activity-indicator');
+    expect(activityIndicator).toBeNull()
   });
-})
+});
