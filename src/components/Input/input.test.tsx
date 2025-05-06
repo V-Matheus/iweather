bb3c67a55696e0b2654e01d7cbe625907b4f4a5a
Victor Matheus <victormatheus507@gmail.com>
2025-05-06 15:54:12 -0300

    render component in test

---
+++ src/components/Input/input.test.tsx
@@ -0,0 +1,8 @@
+import {render} from '@testing-library/react-native';
+import { Input } from '.';
+
+describe('Component: Input', () => {
+  it('should be render without activity indicator', () => {
+    render(<Input />);
+  });
+})
