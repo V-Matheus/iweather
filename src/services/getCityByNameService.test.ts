c6352483f18e128ee70971f3c1237781c226d58d
Victor Matheus <victormatheus507@gmail.com>
2025-05-14 16:06:07 -0300

    test api return

--- src/services/getCityByNameService.test.ts
+++ src/services/getCityByNameService.test.ts
@@ -2,12 +2,12 @@ import { mocCityApiResponse } from '@__tests__/mocks/mockCityApiResponse';
 import { api } from './api';
 import { getCityByNameService } from './getCityByNameService';
 
-describe('API: getCityByNameService', () => {
+describe('Service: getCityByNameService', () => {
   it('should be return city details', async () => {
     jest.spyOn(api, 'get').mockResolvedValue({
       data: mocCityApiResponse,
     });
-    
+
     const response = await getCityByNameService('São Paulo');
     expect(response).toEqual([
       { id: '1', latitude: 123, longitude: 456, name: 'São Paulo, BR' },
