937db2dac596db6ec07998bc656d710736e604e9
Victor Matheus <victormatheus507@gmail.com>
2025-05-20 17:29:29 -0300

    WaitFor e act

--- src/contexts/CityContext.test.tsx
+++ src/contexts/CityContext.test.tsx
@@ -1,12 +1,15 @@
 import { useCity } from '@hooks/useCity';
-import { renderHook } from '@testing-library/react-native';
+import { act, renderHook, waitFor } from '@testing-library/react-native';
 import { CityProvider } from '@contexts/CityContext';
 
 describe('Context: CityContext', () => {
   it('should be change selected city', async () => {
     const { result } = renderHook(() => useCity(), { wrapper: CityProvider });
 
-    console.log(result.current);
-    
+    await waitFor(() =>
+      act(() => {
+        console.log(result.current);
+      }),
+    );
   });
 });
