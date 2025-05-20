a020843580ade2b4d1bde1180d481ec29d6ca7ae
Victor Matheus <victormatheus507@gmail.com>
2025-05-20 16:46:45 -0300

    saveStorageCity

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -1,8 +1,23 @@
-import { getStorageCity } from './cityStorage';
+import { CityProps } from '@services/getCityByNameService';
+import { getStorageCity, saveStorageCity } from './cityStorage';
 
 describe('Storage: CityStorage', () => {
   it('should be return null when don´t have city storaged', async () => {
     const response = await getStorageCity();
     expect(response).toBeNull();
+  });
+
+  it('should be return a city when have city storaged', async () => {
+    const newCity: CityProps = {
+      id: '1',
+      name: 'São Paulo',
+      latitude: 123,
+      longitude: 456,
+    };
+
+    await saveStorageCity(newCity);
+    const response = await getStorageCity();
+
+    expect(response).toEqual(newCity);
   });
 });
